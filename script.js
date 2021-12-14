1dfcfa79b2f72e319c2ebc6c533204d237c8357d
tamalweb <tamalweb@protonmail.com>
2021-12-14 18:45:28 +0600

    ⚡Made it working with skeleton loading

--- script.js
+++ script.js
@@ -1,117 +1,61 @@
-$(
-    () => {
-        // Get data from live site
-        const endPoint = 'https://api.coinmarketcap.com/v1/ticker/?limit=10';
-        const localSource = 'data/sample.json'
-        const numberWithCommas = (x) => {
-          return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        }
+import sampleData from './data/data.js'
 
+const API_KEY = '4E6351C9-384C-4A47-B7A6-7E1E9651FEF6'
+const endPoint =
+  'https://rest-sandbox.coinapi.io/v1/assets?type_is_crypto=1&apikey=' + API_KEY
 
-        $.get(endPoint, (data) => {
-            data.forEach((item) => {
-                let price = item.price_usd;
-                let change = item.percent_change_24h;
-                let change_percent = Math.sign(change);
+const LOCAL_MODE = true // So I don't keep using api requests
 
-                console.log(change_percent);
+$(() => {
+  // Get data from live site
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  }
 
-                if (price < 100) {
-                    price = parseFloat(Math.round(price * 100) / 100).toFixed(2);
-                } else {
-                    price = Math.round(price)
-                    price = numberWithCommas(price)
-                }
+  function populateResults(data) {
+    data.forEach((item, index) => {
+      let price = item.price_usd || 1
+      let change = item.percent_change_24h || 'N/A'
+      let change_percent = Math.sign(change)
 
-                var changeClass;
-                var fontAwesome;
+      if (price < 100) {
+        price = parseFloat(Math.round(price * 100) / 100).toFixed(2)
+      } else {
+        price = Math.round(price)
+        price = numberWithCommas(price)
+      }
 
-                if(change_percent === 1) {
-                    changeClass = 'green'
-                    fontAwesome = 'fas fa-arrow-up'
-                } else {
-                    changeClass = 'red'
-                }
+      let changeClass
+      let fontAwesome
 
+      if (change_percent === 1) {
+        changeClass = 'green'
+        fontAwesome = 'fas fa-arrow-up'
+      } else {
+        changeClass = 'red'
+      }
 
+      document
+        .querySelectorAll('.skeleton')
+        .forEach((el) => (el.style.display = 'none'))
 
+      $('ul').append(`<li class="item">
+                      <span class="currency">${index + 1}. ${item.name}</span>
+                      <span class="price">$${price}</span>
+                      <span class="change">
+                      ${change}</span></li>`)
+    })
+  }
 
-                $("ul").append(`<li class="item">
-                    <span class="currency">${item.rank}. ${item.name}</span>
-                    <span class="price">$${price}</span>
-                    <span class="change ${changeClass}">
-                    ${change}%</span></li>`)
+  function getDataFromAPI(apiEndpoint) {
+    $.get(apiEndpoint, (data) => {
+      populateResults(data)
+    })
+  }
 
-
-
-                // if (change_percent === (-1)) {
-                //     $('.change').addClass('red')
-                // }
-
-                console.log(item.name + ": " + item.price_usd);
-
-            })
-        })
-
-    }
-
-
-
-
-
-)
-
-
-
-
-
-// $(() => {
-//
-//         getMessages()
-//
-//         $('.send').click(() => {
-//
-//
-//
-//
-//             // let name = $('.name_field').val()
-//             // let message = $('.message_field').val()
-//             //
-//             // getMessages();
-//             //
-//             // if((name && message) !== '') {
-//             //     addMessages(`{name: ${name}, message: ${message}}`)
-//             //
-//             //     console.log('Clicked');
-//             //     console.log(name + " " + message);
-//             // }
-//
-//
-//
-//         })
-//     })
-//
-//     // const addMessages = () => {
-//     //     $('.name').append(message.name)
-//     //     $('.message').append(message.message)
-//     // }
-//     //
-//     // const getMessages = () => {
-//     //     $.get('/messages', (data) => {
-//     //         addMessages(`{name: ${data.name}, message: ${data.message}}`)
-//     //         console.log(data);
-//     //     })
-//     // }
-//
-//
-//     const getMessages = () => {
-//         $.get('/messages', (data) => {
-//             data.forEach((item) => {
-//                 console.log(item.name + " " + item.message);
-//
-//                 $('.single_message').append(`<h4>${item.name}</h4><p>${item.message}</p>`)
-//                 // $('.message').append(item.message)
-//             })
-//             // console.log({name: data.name, message: data.message});
-//         })
-//     }
+  if (LOCAL_MODE) {
+    populateResults(sampleData)
+  } else {
+    getDataFromAPI(endPoint)
+  }
+})
